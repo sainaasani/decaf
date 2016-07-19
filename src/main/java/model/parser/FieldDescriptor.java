e5cacb775b29b8463d6e2bf1be893c5ffc6da009
vaghari <majid_vaghari2009@yahoo.com>
2016-07-19 17:00:02 +0000

    Add descriptors for use in intermediate forms

---
+++ src/main/java/model/parser/FieldDescriptor.java
@@ -0,0 +1,7 @@
+import model.Type;
+
+public class FieldDescriptor {
+  private String name;
+  private Type type;
+  private int objectOffset;
+}
---
+++ src/main/java/model/parser/LocalDescriptor.java
@@ -0,0 +1,7 @@
+import model.Type;
+
+public class LocalDescriptor {
+  private String name;
+  private Type type;
+  private int stackOffset;
+}
