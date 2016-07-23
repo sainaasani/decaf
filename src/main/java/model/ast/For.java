787d1559f9ec12d218efa4ff7aa9946011af5639
sainaasani <sainaasani123@gmail.com>
2016-07-23 23:41:39 +0430

    Create For.java

---
+++ src/main/java/model/ast/For.java
@@ -0,0 +1,14 @@
+package model.AST;
+
+public class For extends Statement{
+
+	public For(Block parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+	Expression getExpression(){
+		Expression e = null;
+		return e;
+	}
+
+}
